df06621f7e450c7a03167fe532d5ebf01a1ea701
Anthony Hien Vu <anthony.vu@checkbox.ai>
2022-01-01 10:13:00 +1100

    Add mock items.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
 import React from "react";
 import "./App.css";
 
+const mockItems = [
+  { title: "Folder 1" },
+  { title: "Folder 2", children: [{ title: "Folder 2.1" }] },
+  {
+    title: "Folder 3",
+    children: [
+      { title: "Folder 3.1" },
+      { title: "Folder 3.2", children: [{ title: "Folder 3.2.1" }] },
+      {
+        title: "Folder 3.3",
+        children: [
+          { title: "Folder 3.3.1" },
+          { title: "Folder 3.3.2", children: [{ title: "Folder 3.3.2.1" }] },
+        ],
+      },
+    ],
+  },
+];
+
 function App() {
   return <div className="App">Hello World</div>;
 }
