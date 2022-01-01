ba6aa2469d826ce22efe868a35940d0783c375ce
Anthony Hien Vu <anthony.vu@checkbox.ai>
2022-01-02 10:42:58 +1100

    Style list and replace list item bullets with svg icons.

--- src/components/List/Index.tsx
+++ src/components/List/Index.tsx
@@ -1,13 +1,16 @@
 import { IFile, IFolder } from "../../types/global";
 import ClosedFolderIcon from "../Icons/ClosedFolder";
 import FileIcon from "../Icons/File";
+import ExpandArrowIcon from "../Icons/ExpandArrow";
+import styles from "./index.module.scss";
+
 interface IListProps {
   items: (IFolder | IFile)[];
 }
 
 const List = ({ items }: IListProps) => {
   return (
-    <ul>
+    <ul className={styles.unorderedList}>
       {items.map(
         ({
           type,
@@ -15,9 +18,11 @@ const List = ({ items }: IListProps) => {
           children,
         }: (IFile | IFolder) & { children?: (IFile | IFolder)[] }) => {
           return (
-            <li key={`Type:${type} Title:${title}`}>
-              {type === "folder" ? <ClosedFolderIcon /> : <FileIcon />}
-              {`${type}: ${title}`}{" "}
+            <li key={`Type:${type} Title:${title}`} className={styles.listItem}>
+              <span className={styles.lineContent}>
+                {type === "folder" ? <ClosedFolderIcon /> : <FileIcon />}
+                {title}{" "}
+              </span>
               {!!children?.length && <List items={children} />}
             </li>
           );
